5991bce9cf70428f62f3e6041613c49d796e4844
Anthony <cozziad@gmail.com>
2020-05-29 12:47:45 -0400

    gitignore

--- demosite/demosite/settings.py
+++ demosite/demosite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django1',
         'USER': 'postgres',
-        'PASSWORD': 'bob',
+        'PASSWORD': 'mordor##99',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
